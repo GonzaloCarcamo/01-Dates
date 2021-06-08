0f8186aa5121c7221598968a228d57f990e95142
goncmodev <gon.cmo.dev@gmail.com>
2021-06-08 16:26:48 +0100

    Creating Formulario component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
-  return (
+  return(
+    <Fragment>
     <h1>Administración de Pacientes</h1>
+    
+    <div className="container">
+      <div className="row">
+        <div className="one-half column">
+          <Formulario />
+        </div>
+        <div className="one-half column">
+          2
+        </div>
+      </div>
+    </div>
+  </Fragment>
   );
 }
 
