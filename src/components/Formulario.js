443106c3fbf0409c5bd22164ce716d17fd4e439f
goncmodev <gon.cmo.dev@gmail.com>
2021-06-08 16:48:07 +0100

    adding fields in form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Formulario = () => {
         return ( 
-                <h1>Desde Formulario</h1>
+                <Fragment>
+                        <h2>Crear Cita</h2>
+
+                        <form>
+                                <label>Nombre Mascota</label>
+                                <input
+                                        type="text"
+                                        name="mascota"
+                                        className="u-full-width"
+                                        placeholder="nombre mascota"
+                                />
+                                <label>Nombre Dueño</label>
+                                <input
+                                        type="text"
+                                        name="propietario"
+                                        className="u-full-width"
+                                        placeholder="nombre propietario mascota"
+                                />
+
+                                <label>Fecha</label>
+                                <input
+                                        type="date"
+                                        name="fecha"
+                                        className="u-full-width"
+                                />
+
+                                <label>Hora</label>
+                                <input
+                                        type="time"
+                                        name="hora"
+                                        className="u-full-width"
+                                />
+
+                                <label>Síntomas</label>
+                                <textarea
+                                        className="u-full-width"
+                                        name="sintomas"
+                                ></textarea>
+                                <button
+                                        type="button"
+                                        className="u-full-width button-primary"
+                                >Agregar Cita</button>
+                        </form>
+                </Fragment>
          );
 }
  
