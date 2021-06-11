91ffcd355f74c534b2d861d77c2766d9d69534c6
goncmodev <gon.cmo.dev@gmail.com>
2021-06-11 18:51:13 +0100

    PropTypes

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import shortid from 'shortid';
+import PropTypes from 'prop-types';
 
 const Formulario = ({crearCita}) => {
         // Crear state de citas
@@ -117,6 +118,10 @@ const Formulario = ({crearCita}) => {
                         </form>
                 </Fragment>
          );
+}
+
+Formulario.propTypes = {
+        crearCita: PropTypes.func.isRequired
 }
  
 export default Formulario;
